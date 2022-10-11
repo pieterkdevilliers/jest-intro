54d73e574d1df9ba33b0dce0f73c609c90fb5116
pieterkdevilliers <pieterkdevilliers@me.com>
2022-10-11 21:24:19 +0000

    Add h1 test

--- scripts/tests/button.test.js
+++ scripts/tests/button.test.js
@@ -7,7 +7,11 @@
  const buttonClick = require("../button");
 
  beforeEach(() => {
-     document.body.innerHTML = "<p id='par'></p>";
+     let fs = require("fs");
+     let fileContents = fs.readFileSync("index.html", "utf-8");
+     document.open();
+     document.write(fileContents);
+     document.close();
  });
  
  describe("DOM tests", () => {
@@ -16,4 +20,8 @@
          expect(document.getElementById("par")
              .innerHTML).toEqual("You Clicked");
      });
+test("h1 should exist", () => {
+    expect(document.getElementsByTagName("h1").length).toBe(1)
+})
+
  });
